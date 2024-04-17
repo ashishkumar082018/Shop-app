df33bb6d7ed1f779205435d39639c4701de5dee4
ashishkumar082018 <ashishkumar082018@gmail.com>
2024-04-17 10:40:18 +0530

    Task 21 Cart with many to many relationship

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'Ashish@123', {
-    dialect: 'mysql',
-    host: 'localhost'
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
 module.exports = sequelize;
